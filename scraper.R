76b6e723f7d73b0e52decf50390d70eeb6ea5e65
DavidTheBugWriter <DavidTheBugWriter@users.noreply.github.com>
2020-04-13 20:01:17 +0100

    scraper changes
    .gov.uk site text changed. They added tables making scraping more reliable hopefully.

--- scraper.R
+++ scraper.R
@@ -9,33 +9,50 @@
     {
       govurl<-'https://gov.uk/guidance/coronavirus-covid-19-information-for-the-public'
       
-      pat_all<-"^As of 9am on (.*), ([0-9,]+) .* with ([0-9,]+) .* on (.*). Some"
-      pat_pos<-"^([0-9,]+) people .* of whom ([0-9,]+) tested positive."
-      pat_died<-"^As of 5pm on (.*), .* coronavirus, ([0-9,]+) have died."
+      pat_alldate<-"^As of 9am on (.*), [0-9,]+ .*"
+      #pat_pos<-"^([0-9,]+) people .* of whom ([0-9,]+) tested positive."
+      pat_dieddate<-"^As of 5pm on (.*), .* tested positive for coronavirus, [0-9,]+ have died."
       
       covid_data_html <- read_html(govurl) #turn page to xml
       covid_xml<-html_nodes(covid_data_html, '#number-of-cases-and-deaths') 
       
       all_tests_raw_text<-html_text(xml_siblings(covid_xml)[3])
-      positive_raw_text<-html_text(xml_siblings(covid_xml)[4])
+      #positive_raw_text<-html_text(xml_siblings(covid_xml)[4])
       deaths_raw_text<-html_text(xml_siblings(covid_xml)[5])
       
-      all_text<-str_match(all_tests_raw_text,pat_all)
-      positive_text<-str_match(positive_raw_text,pat_pos)
-      death_text<-str_match(deaths_raw_text,pat_died)
+      all_text<-str_match(all_tests_raw_text,pat_alldate)
+      #positive_text<-str_match(positive_raw_text,pat_pos)
+      death_text<-str_match(deaths_raw_text,pat_dieddate)
+      
+      #grab table
+      table_raw<-xml_siblings(covid_xml)[6]
+      table<- table_raw %>% html_table #df
+      
+      tests<- table[[1]]$X2
+      ppl_tested<- table[[1]]$X3
+      pos_tests<-table[[1]]$X4
+      death<-tests<- table[[1]]$X5
       
       # sanity check
       if (NA %in% all_text){
-      flog.error("scraper failed with one or more NA terms, please check")   
+      flog.error("scraper failed with one or more NA terms in 'All_text', please check")
+#      } else if (NA %in% positive_text){
+#      flog.error("scraper failed with one or more NA terms in 'positive_text', please check")
+      } else if (NA %in% death_text){
+      flog.error("scraper failed with one or more NA terms in 'death_text', please check")        
       }
       
       scrape<-data.frame(
         report_date=as.Date(all_text[2],"%d %B"),
-        tested=as.integer(gsub(",","",positive_text[2])),
-        tests_on=data.frame(number=gsub(",","",all_text[4]),date=as.Date(all_text[5],"%d %B")),
-        pos_tested=as.integer(gsub(",","",positive_text[3])),
+        #tested=as.integer(gsub(",","",positive_text[2])),
+        tested=gsub(",","",gsub(",","",ppl_tested[3])),
+        daily_tests=as.integer(gsub(",","",ppl_tested[2])),
+        #tests_on=data.frame(number=gsub(",","",all_text[4]),date=as.Date(all_text[5],"%d %B")),
+        #pos_tested=as.integer(gsu b(",","",positive_text[3])),
+        pos_tested=as.integer(gsub(",","",pos_tests[3])),
         died_date=as.Date(death_text[2],"%d %B"),
-        died=as.integer(gsub(",","",death_text[3]))
+        #died=as.integer(gsub(",","",death_text[3]))
+        died=as.integer(gsub(",","",death[3]))
       )
       return(scrape)
     }
